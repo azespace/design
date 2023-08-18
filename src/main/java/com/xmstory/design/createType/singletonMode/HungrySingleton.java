73271c3b1c20920dcca02dd5d93e41c23b31ca62
Jun Fang <whatdossuperman@gmail.com>
2023-08-18 08:57:06 +0800

    小明,style：规范化

--- src/main/java/com/xmstory/design/createType/singletonMode/HungrySingleton.java
+++ src/main/java/com/xmstory/design/createType/singletonMode/HungrySingleton.java
@@ -3,15 +3,24 @@ package com.xmstory.design.createType.singletonMode;
 /**
  * There is plenty of fish in the sea
  * create by XM on 2023/02/01
- * Description:
+ * Description: 饿汉式 静态全局变量 类加载(程序启动)初始化实例
  **/
-public class HungrySingleton {//饿汉式  类加载(程序启动)初始化实例
-    //1.构造方法私有，防止外部new
+public class HungrySingleton {
+    /**
+     * 造方法私有，防止外部new
+     */
     private HungrySingleton(){
     }
-    //2.内部创建实例
+
+    /**
+     * 内部创建实例
+     */
     private static HungrySingleton hungrySingleton = new HungrySingleton();
-    //3.全局唯一访问实例方法
+
+    /**
+     * 全局唯一访问实例方法
+     * @return
+     */
     public static HungrySingleton getSingleton(){
         return hungrySingleton;
     }
--- src/main/java/com/xmstory/design/createType/singletonMode/LazySingleton.java
+++ src/main/java/com/xmstory/design/createType/singletonMode/LazySingleton.java
@@ -3,16 +3,23 @@ package com.xmstory.design.createType.singletonMode;
 /**
  * There is plenty of fish in the sea
  * create by XM on 2023/02/01
- * Description:
+ * Description: 懒汉式线程不安全(没加sys锁)
  **/
-public class LazySingleton {//懒汉式线程不安全(没加sys锁)
-    //1 构造方法私有，防止外部new
+public class LazySingleton {
+    /**
+     * 构造方法私有，防止外部new
+     */
     private LazySingleton(){}
 
-    //2 内部构建实例，实例化被延迟到调用该方法时创建
+    /**
+     * 内部构建实例，实例化被延迟到调用该方法时创建
+     */
     private static LazySingleton singleton;
 
-    //3 提供全局唯一访问点
+    /**
+     * 提供全局唯一访问点
+     * @return
+     */
     public static LazySingleton getSingleton(){
         //可以明显看到多个线程是可能存在new 多次的情况的，线程不安全。
         if (singleton == null){
